e61ab4c0544d80f2f9ff92e860cd373432dd94da
cx <995141855@qq.com>
2020-05-12 15:43:33 +0800

    结束标志

--- my_proj/monitor/main.go
+++ my_proj/monitor/main.go
@@ -1,6 +1,9 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
 
 /*
 Go有一个语句叫做select，用于监测各个信道的数据流动。
@@ -13,26 +16,49 @@ func foo(i int) chan int {
 	return c
 }
 
+//func main() {
+//	c1, c2, c3 := foo(1), foo(2), foo(3)
+//
+//	c := make(chan int)
+//
+//	go func() { // 开一个goroutine监视各个信道数据输出并收集数据到信道c
+//		for {
+//			select { // 监视c1, c2, c3的流出，并全部流入信道c
+//			case v1 := <-c1:
+//				c <- v1
+//			case v2 := <-c2:
+//				c <- v2
+//			case v3 := <-c3:
+//				c <- v3
+//			}
+//		}
+//	}()
+//
+//	//阻塞主线程，取出信道c的数据
+//	for i := 0; i < 3; i++ {
+//		fmt.Println(<-c) // 从打印来看我们的数据输出并不是严格的1,2,3顺序
+//	}
+//}
+
 func main() {
+	//使用select的时候，有时需要超时处理, 其中的timeout信道相当有趣:
+	timeout := time.After(1 * time.Second) // timeout 是一个计时信道, 如果达到时间了，就会发一个信号出来
+
 	c1, c2, c3 := foo(1), foo(2), foo(3)
 
-	c := make(chan int)
+	//c := make(chan int)
 
-	go func() { // 开一个goroutine监视各个信道数据输出并收集数据到信道c
-		for {
-			select { // 监视c1, c2, c3的流出，并全部流入信道c
-			case v1 := <-c1:
-				c <- v1
-			case v2 := <-c2:
-				c <- v2
-			case v3 := <-c3:
-				c <- v3
-			}
+	for is_timeout := false; !is_timeout; {
+		select {
+		case v1 := <-c1:
+			fmt.Println("received %d from c1", v1)
+		case v2 := <-c2:
+			fmt.Println("received %d from c2", v2)
+		case v3 := <-c3:
+			fmt.Println("received %d from c3", v3)
+		case <-timeout:
+			is_timeout = true // 超时
+			fmt.Println("timeout")
 		}
-	}()
-
-	//阻塞主线程，取出信道c的数据
-	for i := 0; i < 3; i++ {
-		fmt.Println(<-c) // 从打印来看我们的数据输出并不是严格的1,2,3顺序
 	}
 }
